941e65f4e7f971f77868d37f75c7cbd4a6e5d786
it-fm <42847248+it-fm@users.noreply.github.com>
2019-03-30 22:38:22 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,28 @@
+package gobackup
+
+import (
+    "github.com/it-fm/gozip"
+    "github.com/robfig/cron"
+    "fmt"
+    "math/rand"
+    "runtime"
+    "strconv"
+)
+
+func Start() {
+
+    go func() {
+        c := cron.New()
+        //start backup 30 in 30 minutes
+        c.AddFunc("30 * * * *", func() { backup()})
+        c.Start()
+    }()
+
+    runtime.Goexit()
+}
+
+func backup() {
+    number := rand.Intn(500 - 1) + 1
+    fmt.Println("start backup", number)
+    gozip.Zip("./folderBackup", strconv.Itoa(number) + "backupResult.zip")
+}
